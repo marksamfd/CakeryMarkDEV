964fbe420be0c176bd81b94085d84f09776e6a7d
Anas Ahmad Abdelsalam <115808460+Anas-Ah25@users.noreply.github.com>
2024-11-23 16:39:33 +0200

    db configuraton, tested, working with seprate .env file for enviroment variables

---
+++ db.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text  # all imports will be summed up further, wait till map the hiererchy of files
+import secrets
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # load the secret key from .env file, to seperate secret code temporary, also database link is manually implemented to ease testing between us
+
+db = SQLAlchemy()
+
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # my configuration 
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:aabb1122@localhost:5432/Cakery" # manual connection to database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') # from .env file
+    db.init_app(app)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+
+    with app.app_context():
+        try:
+            # Test database connection
+            db.session.execute(text("SELECT 1"))
+            print("Database connection successful!")
+            
+        except Exception as e:
+            print("Database connection failed or query error!")
+            print(f"Error: {e}")
+
+
+# test after running with your configuration 
+'''  
+ # Query to fetch all rows from CustomerUser table
+            results = db.session.execute(
+                text(
+                    """
+                    SELECT customeremail, password, firstname, lastname, phonenum, addressgooglemapurl, createdat
+                    FROM public.customeruser
+                    """
+                )
+            )
+
+            # Enable column key mapping
+            results = results.mappings().all()
+            
+            if results:
+                print("All rows in CustomerUser table:")
+                for row in results:
+                    print(dict(row))  # Convert each mapping to a dictionary
+            else:
+                print("CustomerUser table is empty.")
+
+'''
