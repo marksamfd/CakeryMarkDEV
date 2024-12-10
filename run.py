af34a862e1990a34058ad83f42095209aef34856
Anas Ahmad Abdelsalam <115808460+Anas-Ah25@users.noreply.github.com>
2024-12-10 20:16:24 +0200

    register the blueprints

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app.db import create_app, db
 from app.Controllers.customer_controller import customer_controller
+from app.Controllers.baker_controller import baker_controller
 
 app = create_app()
 app.register_blueprint(customer_controller)
+app.register_blueprint(baker_controller)
 
 if __name__ == "__main__":
     with app.app_context():
