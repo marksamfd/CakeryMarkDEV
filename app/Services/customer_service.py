6d782d92333c914966b667057a6ee0808c31bac3
Anas Ahmad Abdelsalam <115808460+Anas-Ah25@users.noreply.github.com>
2024-12-11 04:23:26 +0200

    added customer serviced methods

--- app/Services/customer_service.py
+++ app/Services/customer_service.py
@@ -1,8 +1,42 @@
 from app.Repositories.customer_repository import CustomerRepository
+from app.Repositories.order_repository import OrderRepository
 
 class CustomerService:
     def __init__(self):
         self.customer_repo = CustomerRepository()
+        self.order_repo = OrderRepository()
 
-    def get_all_products(self):
+
+    # ----------------- get all products ---------------------
+    def list_products(self):
         return self.customer_repo.get_all_products()
+
+    # ------------------- product detail -------------------
+    def view_product_details(self, product_id):
+        return self.customer_repo.get_product_by_id(product_id)
+
+    # ------------------- get cart data -------------------
+    def view_cart(self, customer_email):
+        return self.customer_repo.get_cart(customer_email)
+    # ------------------- add to cart -------------------
+    def add_to_cart(self, customer_email, product_id, quantity):
+        return self.customer_repo.add_item_to_cart(customer_email, product_id, quantity)
+    # ------------------- remove from cart -------------------
+    def remove_from_cart(self, customer_email, product_id):
+        return self.customer_repo.remove_from_cart(customer_email, product_id)
+    # ------------------- make custom cake  -------------------
+    def create_custom_cake(self, customer_email, data):
+        return self.customer_repo.create_custom_cake(customer_email, data)
+    # ------------------- view raw materials -------------------
+    def view_raw_materials(self):
+        return self.customer_repo.get_raw_materials()
+    # ------------------- make order -------------------
+    def checkout(self, customer_email, voucher_code):
+        cart = self.customer_repo.get_cart(customer_email)
+        if "error, cart not found" in cart:
+            return cart  # Return error if cart not found
+        cart_items = cart.get("items", [])
+        return self.order_repo.create_order(customer_email, cart_items, voucher_code)
+    # ------------------- view customer orders -------------------
+    def view_customer_orders(self, customer_email):
+        return self.order_repo.get_orders_by_customer(customer_email)
