e1b8357fd118b5d39611762b33b5830ac236bfd8
Anas Ahmad Abdelsalam <115808460+Anas-Ah25@users.noreply.github.com>
2024-12-11 04:14:45 +0200

    refactor order service: add commented, def, tested main parts: create_order_from_cart, update_order_status, and assign_delivery methods

--- app/Services/order_service.py
+++ app/Services/order_service.py
@@ -5,12 +5,12 @@ class OrderService:
     def __init__(self):
         self.order_repo = OrderRepository()
         self.delivery_repo = DeliveryRepository()
-
-    def create_order_from_cart(self, customer_email, cart_items, voucher_code=None):
-        return self.order_repo.create_order(customer_email, cart_items, voucher_code)
-
-    def update_order_status(self, order_id, status):
-        return self.order_repo.update_order_status(order_id, status)
-
-    def assign_delivery(self, order_id, delivery_email):
-        return self.delivery_repo.assign_delivery_user(order_id, delivery_email)
+    # ----------- make order -------------------
+    def create_order_from_cart(self,customer_email,cart_items,voucher_code=None):
+        return self.order_repo.create_order(customer_email,cart_items,voucher_code)
+    # ----------- update order status -------------------
+    def update_order_status(self,order_id,status):
+        return self.order_repo.update_order_status(order_id,status)
+    # --------------- assign delivery ---------------
+    def assign_delivery(self,order_id,delivery_email): # called in bakery_service on status = "prepared"
+        return self.delivery_repo.assign_delivery_user(order_id,delivery_email)
