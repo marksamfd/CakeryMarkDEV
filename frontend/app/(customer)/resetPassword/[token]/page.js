e53fcb29bb234b5f51bab48b2b3990c4e9206231
Mark Samuel <marksamfd511@gmail.com>
2024-12-17 06:03:10 +0200

    forgot password request initial

---
+++ frontend/app/(customer)/resetPassword/[token]/page.js
@@ -0,0 +1,23 @@
+
+
+function resetPassword({params}) {
+    const token = (await params).token;
+    async function resetPassswordForm(fd){
+        
+
+        const order = await (
+          await fetch(`${process.env.backend}/user/baker/orders`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+          })
+        ).json();
+    }
+    return (
+
+    )
+}
+
+export default resetPassword
