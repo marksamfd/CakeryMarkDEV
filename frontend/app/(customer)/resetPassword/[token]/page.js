25135c92ee8735eb98f426b1d537c573119098d5
Mark Samuel <marksamfd511@gmail.com>
2024-12-18 19:04:17 +0200

    password UI design

--- frontend/app/(customer)/resetPassword/[token]/page.js
+++ frontend/app/(customer)/resetPassword/[token]/page.js
@@ -1,23 +1,36 @@
+import Button from '../../components/button';
+import CheckoutInputField from '../../components/checkoutInput';
+import Title from '../../components/title';
 
-
-function resetPassword({params}) {
-    const token = (await params).token;
-    async function resetPassswordForm(fd){
-        
-
-        const order = await (
-          await fetch(`${process.env.backend}/user/baker/orders`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-          })
-        ).json();
-    }
-    return (
-<></>
-    )
+async function resetPassword({ params }) {
+  const token = (await params).token;
+  async function resetPassswordForm(fd) {
+    'use server';
+    const order = await (
+      await fetch(`${process.env.backend}/user/baker/orders`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+    ).json();
+  }
+  return (
+    <div className="container">
+      <Title>Reset your Password</Title>
+      <from action={resetPassswordForm} className="d-flex flex-column">
+        <CheckoutInputField name={'password'} label={'Password'} />
+        <CheckoutInputField
+          name={'confirmPassword'}
+          label={'Confirm Password'}
+        />
+        <Button type={'submit'} classNameProp={'w-100'}>
+          Submit
+        </Button>
+      </from>
+    </div>
+  );
 }
 
-export default resetPassword
+export default resetPassword;
