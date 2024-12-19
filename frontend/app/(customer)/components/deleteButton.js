919ec6e17849d8d9cb8507cba6c5807f80ca409f
Tasneem-Mohammed <tasneem.abdelgaleel@gmail.com>
2024-12-19 19:47:08 +0200

    Fixed deleting operation in View Staff page

--- frontend/app/(customer)/components/deleteButton.js
+++ frontend/app/(customer)/components/deleteButton.js
@@ -1,32 +1,36 @@
-'use client';  
+'use client';
 import React from 'react';
 async function handleDeleteClick(userEmail, role, token) {
-    await fetch(`/api/cakery/user/admin/Staff/Delete`, {
-      method: 'DELETE',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: userEmail,  
-        role: role
-      }),
+    const response = await fetch(`/api/cakery/user/admin/Staff/Delete`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: userEmail,
+            role: role
+        }),
       
     });
-    console.log(userEmail,role);
-
-  }
+    if (response.ok) {
+        alert('User deleted successfully');
+        window.location.reload(); //
+    } else {
+        alert('Error deleting user');
+    }
+}
 const DeleteButton = ({ userEmail, role, token }) => {
-  return (
-    <button
-      onClick={() => handleDeleteClick(userEmail, role, token)}
-      className="btn btn-light"
-      style={{ marginRight: '10px' }}
-    >
-      <i className="fa fa-trash" style={{ color: 'red' }}></i>
-    </button>
-  );
+    return (
+        <button
+            onClick={() => handleDeleteClick(userEmail, role, token)}
+            className="btn btn-light"
+            style={{ marginRight: '10px' }}
+        >
+            <i className="fa fa-trash" style={{ color: 'red' }}></i>
+        </button>
+    );
 };
 
 export default DeleteButton;
